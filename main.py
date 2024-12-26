da427dc2173363ec144b7d4299d94d05b261f472
Kuldeepbhardwaj052 <kuldeepbhardwaj3355@gamil.com>
2024-12-26 16:03:12 +0530

    Basic Claculator

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("My name is Jay")
+
+
+# input Commands 
+
+a=int(input("Enter your first number"))
+b=int(input("Enter your second number"))
+add=a+b
+print("Addition will be:",add)
+sub=a-b
+print("subtraction will be :",sub)
+mul=a*b
+print("Multiplication will be :",mul)
+div=a/b
+print("Division will be :",div)
+exp=a**b
+print("Exponential will be :",exp)
+mod=a%b
+print("Modulous will be :",mod)
+floor=a//b
+print("Floor Division will be :",floor)
