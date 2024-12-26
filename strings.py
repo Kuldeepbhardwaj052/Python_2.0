947a04321e5d7bcafb4da4506c26101e2b3600ab
Kuldeepbhardwaj052 <kuldeepbhardwaj3355@gamil.com>
2024-12-26 23:47:07 +0530

    String lacture

--- strings.py
+++ strings.py
@@ -1,3 +1,11 @@
-name="harry"
-age=11
-print("hello" + name,age)
+# name=" harry bhaiya"
+# # age=11
+# # print("hello" + name,age)
+# # apple='hello \"harry" '
+# # print(apple)
+# # print(len(name))
+# # print(name[-1])
+# print(name.count("a"))
+
+import timestamp
+
