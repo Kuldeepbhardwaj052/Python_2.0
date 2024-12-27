ef60c709b0cfceb6f78f8b6ecb43deadcc8fe4e8
Kuldeepbhardwaj052 <kuldeepbhardwaj3355@gamil.com>
2024-12-27 14:08:36 +0530

    Some questions

---
+++ loops.py
@@ -0,0 +1,77 @@
+# Example 1: Print the first 10 natural numbers using for loop.
+# for i in range(1,11):
+#     print(i)
+
+
+
+
+# Example 2: Python program to print all the even numbers within the given range.
+
+# for i in range(2,20,2):
+#     print(i)
+
+
+
+# Example 3: Python program to calculate the sum of all numbers from 1 to a given number.
+# n=int(input("enter the no"))
+# sum=0
+# for i in range(1,n+1):
+#     sum=sum+i
+#     print(sum)
+#     i=i+1
+
+# Example 4: Python program to calculate the sum of all the odd numbers within the given range.
+
+# sum=0
+# for i in range(5):
+#     if i%2!=0:
+#         sum=sum+i
+#         print(sum)
+
+# Example 5: Python program to print a multiplication table of a given number
+
+# n=int(input("enter the no."))
+# for i in range(1,11):
+#     print(n*i)
+
+# Example 6: Python program to display numbers from a list using a for loop.
+# list=[1,2,3,4]
+# for i in list:
+#     print(i)
+# Example 7: Python program to count the total number of digits in a number.
+
+# n=str("12344")
+# count=0
+# for i in n:
+#     count=count+1
+# print(count)
+# Example 8: Python program to check if the given string is a palindrome.
+
+# Example 9: Python program that accepts a word from the user and reverses it.
+
+# Example 10: Python program to check if a given number is an Armstrong number
+
+# Example 11: Python program to count the number of even and odd numbers from a series of numbers.
+n=int(input("Enter the no"))
+even=0
+odd=0
+for i in range(1,n+1):
+    if(i%2==0):
+      print(i,"Is even") 
+    elif(i%2!=0):
+       print(i,"Is odd")
+ 
+
+# Example 12: Python program to display all numbers within a range except the prime numbers.
+
+# Example 13: Python program to get the Fibonacci series between 0 to 50.
+
+# Example 14: Python program to find the factorial of a given number.
+
+# Example 15: Python program that accepts a string and calculates the number of digits and letters.
+
+# Example 16: Write a Python program that iterates the integers from 1 to 25.
+
+# Example 17: Python program to check the validity of password input by users.
+
+# Example 18: Python program to convert the month name to a number of days.
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-print("My name is Jay")
+# print("My name is Jay")
 
 
-# input Commands 
+# # input Commands 
 
-a=int(input("Enter your first number"))
-b=int(input("Enter your second number"))
-add=a+b
-print("Addition will be:",add)
-sub=a-b
-print("subtraction will be :",sub)
-mul=a*b
-print("Multiplication will be :",mul)
-div=a/b
-print("Division will be :",div)
-exp=a**b
-print("Exponential will be :",exp)
-mod=a%b
-print("Modulous will be :",mod)
-floor=a//b
-print("Floor Division will be :",floor)
+# a=int(input("Enter your first number"))
+# b=int(input("Enter your second number"))
+# add=a+b
+# print("Addition will be:",add)
+# sub=a-b
+# print("subtraction will be :",sub)
+# mul=a*b
+# print("Multiplication will be :",mul)
+# div=a/b
+# print("Division will be :",div)
+# exp=a**b
+# print("Exponential will be :",exp)
+# mod=a%b
+# print("Modulous will be :",mod)
+# floor=a//b
+# print("Floor Division will be :",floor)
+
+
+t1=(1,2)
+t2=(3,4)
+print(t1+t2)
